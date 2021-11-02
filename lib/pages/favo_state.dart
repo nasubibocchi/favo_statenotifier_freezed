f8054bc87f6b5c88809691eeec3589b7681a8498
nasubibocchi <matsuhito125@gmail.com>
2021-11-02 09:24:10 +0900

    お気に入りボタンをfreezedで実装

--- lib/pages/favo_state.dart
+++ lib/pages/favo_state.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
-// part 'favo_state.g.dart';
 part 'favo_state.freezed.dart';
 
 @freezed
@@ -11,5 +10,4 @@ class FavoState with _$FavoState {
   const factory FavoState(
       {@Default([]) List<Contents> data}) = _FavoState;
 
-// factory FavoState.fromJson(Map<String, dynamic> json) => _$FavoStateFromJson(json);
 }
